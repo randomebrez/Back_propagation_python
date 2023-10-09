067a616e7fd117e8c9cde7d6471b164b724d66ad
randomebreze <louviauxnicolas@gmail.com>
2023-10-09 18:34:06 +0200

    Add conv_net to test_fct file

--- TestLibraryFunction.py
+++ TestLibraryFunction.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 import Manager as manager
 import Tools.PlotHelper as ph
 import Tools.Computations as computer
@@ -39,6 +40,8 @@ def test_dense_1_hidden():
         learning_rate_steps=5,
         epochs=30)
 
+    start = time.time()
+
     # Run model
     pre_train_result = manager.test_network(ds_inputs, ds_targets, network, model_parameters)
     train_results = manager.train_network(ds_inputs, ds_targets, network, model_parameters)
@@ -46,6 +49,9 @@ def test_dense_1_hidden():
 
     # Plot results
     ph.plot_perceptron_result(pre_train_result, train_results['batch_costs'], train_results['mean_batch_costs'], post_train_test)
+
+    tick = time.time()
+    print("FFT network execution time : {0}".format(tick - start))
     return network
 
 
@@ -85,6 +91,8 @@ def test_auto_encoder():
         learning_rate_steps=5,
         epochs=20)
 
+    start = time.time()
+
     # Run model
     pre_train_result = manager.test_network(ds_inputs, ds_inputs, network, model_parameters, with_details=False)
     train_results = manager.train_network(ds_inputs, ds_inputs, network, model_parameters)
@@ -92,4 +100,50 @@ def test_auto_encoder():
 
     # Plot results
     ph.plot_auto_encoder_results(network, ds_inputs[0], pre_train_result, train_results['batch_costs'], train_results['mean_batch_costs'], post_train_test, 36)
+
+    tick = time.time()
+    print("FFT network execution time : {0}".format(tick - start))
+    return network
+
+def test_conv_net():
+    dataset_id = 40996
+    feature_name = 'class'
+    class_number = 10
+    normalization_constant = 255
+    batch_size = 100
+    ds_inputs, ds_targets, input_shape, output_shape = manager.get_image_dataset(dataset_id, feature_name, class_number, normalization_constant, batch_size=batch_size)
+
+    network_builder = builder.NetworkBuilder(input_shape, output_shape)
+
+    network_builder.add_conv_fft_layer(3, 6, 2, 'relu')
+    # network_builder.add_pool_layer(2, 'max')
+    network_builder.add_flat_layer()
+
+    network_builder.add_dense_layer(800)
+    network_builder.add_one_to_one_layer('relu')
+
+    network_builder.add_dense_layer(10)
+    network_builder.add_one_to_one_layer('softmax', is_output_layer=True)
+    network = network_builder.build()
+
+    train_model = model.ModelParameters(
+        computer.cross_entropy,
+        computer.cross_entropy_derivative,
+        initial_learning_rate=0.1,
+        final_learning_rate=0.01,
+        learning_rate_steps=5,
+        epochs=10)
+
+    start = time.time()
+
+    # Run model
+    pre_train_result = manager.test_network(ds_inputs, ds_targets, network, train_model)
+    train_results = manager.train_network(ds_inputs, ds_targets, network, train_model)
+    post_train_test = manager.test_network(ds_inputs, ds_targets, network, train_model)
+
+    # Plot results
+    ph.plot_perceptron_result(pre_train_result, train_results['batch_costs'], train_results['mean_batch_costs'], post_train_test)
+
+    tick = time.time()
+    print("FFT network execution time : {0}".format(tick - start))
     return network
